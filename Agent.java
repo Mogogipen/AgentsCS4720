c94c795202cb38e83657daf927d52e2a7d66846f
Nickolas Reimschiissel <ryming.chizel@gmail.com>
2020-10-09 16:25:35 -0600

    Added State class & Started BFS
    +State class (map, position, movement, and hashing)
    +BFS algorithm (started)
    +Finished run()
    +Enum for Agent search type

--- Agent.java
+++ Agent.java
@@ -1,30 +1,24 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 
 public class Agent implements Runnable {
 	
-	public final int BFS = 101;
-	public final int DFS = 102;
-	
-	private HashMap<String[][], Boolean> nodes;
+	private HashMap<String, Boolean> stateHash;
 	private LinkedList<AgentAction> actionQueue;
 	private GameObject player;
-	private String[][] currentMap;
+	private State map;
 	
 	private Boolean finished;
-	private int searchType;
+	private Search type;
 	
-	Agent(GameObject p, String[][] map, int st) {
+	Agent(GameObject p, String[][] m, Search t) {
 		player = p;
-		currentMap = new String[map.length][map[0].length];
-		for (int i = 0; i < currentMap.length; i++) {
-			for (int j = 0; j < currentMap[i].length; j++) {
-				currentMap[i][j] = new String(map[i][j]);
-			}
-		}
-		searchType = st;
-		nodes = new HashMap<String[][], Boolean>();
+		map = new State(m);
+		type = t;
+		stateHash = new HashMap<String, Boolean>();
 		actionQueue = new LinkedList<AgentAction>();
+		finished = false;
 	}
 	
 	public AgentAction nextAction() {
@@ -39,6 +33,32 @@ public class Agent implements Runnable {
 	}
 	
 	private void BFS() {
+		LinkedList<State> searchQueue = new LinkedList<State>();
+		State currentState = new State(map);
+		while(!finished) {
+			// Check if current node is goal state
+			currentState.isGoalState();
+			
+			// Add new nodes
+			ArrayList<State> add = new ArrayList<State>();
+			add.add(currentState.moveDown());
+			add.add(currentState.moveUp());
+			add.add(currentState.moveRight());
+			add.add(currentState.moveLeft());
+			add.add(currentState.pickUp());
+			for (State s : add) {
+				String hash = s.toHash();
+				if (s != null && !stateHash.containsKey(hash) ) {
+					stateHash.put(hash, true);
+					searchQueue.add(s);
+				}
+			}
+			
+			currentState = searchQueue.pop();
+		}
+	}
+	
+	private void DFS() {
 		
 	}
 	
@@ -48,8 +68,121 @@ public class Agent implements Runnable {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		if (type == Search.BFS)
+			BFS();
+		else if (type == Search.DFS)
+			DFS();
+		else
+			System.out.println("Search parameter failure");
+		finished = true;
 		return;
 	}
 
+}
+
+class State {
+	private String[][] map;
+	private int xPos;
+	private int yPos;
+	
+	//
+	// Constructors
+	//
+	
+	State(String[][] m) {
+		map = new String[m.length][m[0].length];
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[i].length; j++) {
+				if (m[i][j].equals("S")) {
+					xPos = i;
+					yPos = j;
+					map[i][j] = " ";
+				} else
+					map[i][j] = new String(m[i][j]);
+			}
+		}
+	}
+	
+	State(State s) {
+		map = new String[s.map.length][s.map[0].length];
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[i].length; j++) {
+				map[i][j] = new String(s.map[i][j]);
+			}
+		}
+		xPos = s.xPos;
+		yPos = s.yPos;
+	}
+	
+	private State(State s, int x, int y) {
+		this(s);
+		xPos = x;
+		yPos = y;
+	}
+	
+	//
+	// Movement
+	//
+	
+	public State moveUp() {
+		if (map[xPos][yPos+1].equals("w")) {
+			return null;
+		}
+		return new State(this, xPos, yPos+1);
+	}
+	public State moveDown() {
+		if (map[xPos][yPos-1].equals("w"))
+			return null;
+		yPos--;
+		return new State(this, xPos, yPos-1);
+	}
+	public State moveLeft() {
+		if (map[xPos-1][yPos].equals("w"))
+			return null;
+		xPos--;
+		return new State(this, xPos-1, yPos);
+	}
+	public State moveRight() {
+		if (map[xPos+1][yPos].equals("w"))
+			return null;
+		xPos++;
+		return new State(this, xPos+1, yPos);
+	}
+	public State pickUp() {
+		if (map[xPos][yPos].equals(".")) {
+			State result = new State(this);
+			result.map[xPos][yPos] = " ";
+			return result;
+		}
+		return null;
+	}
+	
+	//
+	// Miscellaneous
+	//
+	
+	public String toHash() {
+		String result = ("" + xPos) + yPos;
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[0].length; j++) {
+				result += map[i][j];
+			}
+		}
+		return result;
+	}
+	
+	public boolean isGoalState() {
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[0].length; j++) {
+				if (map[i][j].equals("."))
+					return false;
+			}
+		}
+		return true;
+	}
+}
+
+enum Search {
+	BFS,
+	DFS
 }
