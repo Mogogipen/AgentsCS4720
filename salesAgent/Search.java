a43e0323a645b65d38f6611d226e03f33b8e2175
Nickolas Reimschiissel <ryming.chizel@gmail.com>
2020-10-27 07:31:52 -0600

    Started the assignment
    +getTotalDistance()
    +distance() !Not finished
    +hillClimbingSearch() !Not finished

--- salesAgent/Search.java
+++ salesAgent/Search.java
@@ -17,6 +17,8 @@ public class Search {
 //		states.add(c);
 //		return states;
 		
+//		hillClimbingSearch(cities);
+		
 		//This returns quite a few
 		for(int i = 0; i < 10; i++) {
 			//Create a copy
@@ -30,6 +32,34 @@ public class Search {
 			states.add(tmp.toArray(tmpArray));
 		}
 		return states;
+	}
+	
+	public static ArrayList<City []> hillClimbingSearch(City[] cities) {
+		ArrayList<City []> states = new ArrayList<City []>();
+		ArrayList<City []> checkStates = new ArrayList<City []>();
+		
+		boolean isBest = false;
+		while(!isBest) {
+			isBest = true;
+		}
+		
+		return states;
+	}
+	
+	private static int getTotalDistance(City[] cities) {
+		int result = 0;
+		for (int i = 0; i < cities.length; i++) {
+			if (i == cities.length)
+				result += distance(cities[i], cities[0]);
+			else
+				result += distance(cities[i], cities[i+1]);
+		}
+		return result;
+	}
+	
+	private static int distance(City a, City b) {
+		int result = 0;
+		return result;
 	}
 
 }
